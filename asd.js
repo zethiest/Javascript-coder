e011c124f7d394aea9112fd62630057739f5d4d6
Franco Lietz <francolietz@gmail.com>
2025-03-17 21:26:15 -0300

    Segunda preentrega

---
+++ asd.js
@@ -0,0 +1,6 @@
+// const inicio = new Date()
+// for (let i = 0; i < 1000; i++) {
+//     console.log("haciendo tiempo")
+// }
+// const final = new Date()
+// console.log("El proceso tardó: " + (final - inicio) + " milisegundos")
--- main.js
+++ main.js
@@ -1,26 +1,45 @@
  // Palabra reservada + nombre = dato 
 
+
+
+const Product = function (nombre, precio,id){
+this.nombre = nombre
+this.precio = precio
+this.id = id
+}
+let producto1 = new Product ("Lavarropas", 10000, 1)
+let producto2 = new Product ("Microondas", 5000, 2)
+let producto3 = new Product ("Aspiradora", 8000, 3)
+let producto4 = new Product ("Hidrolavadora", 9500, 4)
+
+const productlist = [producto1,producto2,producto3,producto4]
+
 function subasta (){
-alert ("Subasto este producto, el precio base es 5000")
-let PrecioBase = 5000
-do {
-let Identificacion = (prompt("Para ofertar, es necesario que sepamos tu nombre")).toUpperCase();
-let ofertadelUsuario = parseFloat(prompt("Ingrese su oferta: "))
-if (Identificacion === null || Identificacion.trim() === "" || ofertadelUsuario === null || isNaN (ofertadelUsuario)) {
+productlist.map(product => {
+    alert(`Producto: ${product.nombre}\nPrecio: $${product.precio}\nID: ${product.id}`);
+});
+let Identificacion = (prompt("Para comprar, es necesario que sepamos tu nombre")).toUpperCase();
+let idSelection = parseInt(prompt("Ingrese el ID del producto que desea comprar:"));
+if (Identificacion === null || Identificacion.trim() === "" || idSelection === productlist.find(p => p)) {
     alert ("Que haces?")
-    break;
+    return;
 }
-if (ofertadelUsuario < 4000) {
-alert ("Te pensas que estamos boludeando flaco?")}
-else if (ofertadelUsuario < PrecioBase){
-alert ("Pone unos pesos mas y hablamos cabezon")}
-else if (ofertadelUsuario > PrecioBase){
-alert ("Por fin alguien que pone la tarasca, felicidades maestro, te lo llevaste")
-break}
-else if (ofertadelUsuario === PrecioBase){ 
-alert ("Loco, esta bien que sea el precio de arranque pero poneme unos pesos de mas al menos")
+let productoElegido = productlist.find(p => p.id === idSelection);
+if (!productoElegido) {
+    alert ("Ese producto no existe flaco");
+    return;
 }
+let ofertaUsuario = parseFloat(prompt(`Ingresa su oferta para el ${productoElegido.nombre}`))
+if (isNaN(ofertaUsuario) || ofertaUsuario <= 0) {
+alert ("Flaco, esa oferta no es valida")
+return;
 }
-while (true)
+if (ofertaUsuario < productoElegido.precio){
+alert ("Todo bien loco, pero con eso no llegas, fijate bien")
+}
+else if (ofertaUsuario >= productoElegido.precio)
+
+alert ("Joya maestro, hablame y coordinamos el envio")
+
 }
 subasta()
